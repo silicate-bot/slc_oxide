6dcb047b463944760fe3b9375deddaf9396d2ec5
Silver Slinko <sislin@taltech.ee>
2026-08-01 01:25:26 +0300

    preserve future extended atoms (slop)

--- src/v3/atom.rs
+++ src/v3/atom.rs
@@ -28,6 +28,8 @@ pub enum AtomError {
     IOError(#[from] std::io::Error),
     #[error("Unknown atom ID: {0}")]
     UnknownAtomId(u32),
+    #[error("Atom body is too large")]
+    AtomTooLarge,
     #[error("Section error: {0}")]
     SectionError(#[from] crate::v3::section::SectionError),
 }
@@ -40,44 +42,35 @@ pub trait Atom: Sized {
     fn write<W: Write>(&self, writer: &mut W) -> Result<(), AtomError>;
 }
 
-pub struct NullAtom {
-    pub size: usize,
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct OpaqueAtom {
+    pub id: u32,
+    pub flags: u8,
+    pub body: Vec<u8>,
 }
 
-impl Atom for NullAtom {
-    const ID: AtomId = AtomId::Null;
-
-    fn size(&self) -> usize {
-        self.size
-    }
-
-    fn read<R: Read>(reader: &mut R, size: usize) -> Result<Self, AtomError> {
-        let mut buf = vec![0u8; size];
-        reader.read_exact(&mut buf)?;
-        Ok(Self { size })
-    }
-
-    fn write<W: Write>(&self, _writer: &mut W) -> Result<(), AtomError> {
-        Ok(())
+impl OpaqueAtom {
+    pub fn new(id: u32, flags: u8, body: Vec<u8>) -> Self {
+        Self { id, flags, body }
     }
 }
 
 pub enum AtomVariant {
-    Null(NullAtom),
+    Opaque(OpaqueAtom),
     Action(super::builtin::ActionAtom),
 }
 
 impl AtomVariant {
-    pub fn id(&self) -> AtomId {
+    pub fn id(&self) -> u32 {
         match self {
-            AtomVariant::Null(_) => AtomId::Null,
-            AtomVariant::Action(_) => AtomId::Action,
+            AtomVariant::Opaque(a) => a.id,
+            AtomVariant::Action(_) => AtomId::Action as u32,
         }
     }
 
     pub fn size(&self) -> usize {
         match self {
-            AtomVariant::Null(a) => a.size(),
+            AtomVariant::Opaque(a) => a.body.len(),
             AtomVariant::Action(a) => a.size(),
         }
     }
@@ -86,32 +79,47 @@ impl AtomVariant {
         let mut buf = [0u8; 4];
         reader.read_exact(&mut buf)?;
         let id = u32::from_le_bytes(buf);
-        let atom_id = AtomId::try_from(id)?;
 
         let mut buf8 = [0u8; 8];
         reader.read_exact(&mut buf8)?;
-        let size = u64::from_le_bytes(buf8) as usize;
+        let size_and_flags = u64::from_le_bytes(buf8);
+        let flags = (size_and_flags >> 56) as u8;
+        let size = usize::try_from(size_and_flags & 0x00ff_ffff_ffff_ffff)
+            .map_err(|_| AtomError::AtomTooLarge)?;
 
-        match atom_id {
-            AtomId::Null => Ok(AtomVariant::Null(NullAtom::read(reader, size)?)),
-            AtomId::Action => Ok(AtomVariant::Action(super::builtin::ActionAtom::read(
-                reader, size,
-            )?)),
-            AtomId::Marker => Ok(AtomVariant::Null(NullAtom::read(reader, size)?)),
+        match id {
+            id if id == AtomId::Action as u32 => Ok(AtomVariant::Action(
+                super::builtin::ActionAtom::read(reader, size)?,
+            )),
+            _ => {
+                let mut body = vec![0u8; size];
+                reader.read_exact(&mut body)?;
+                Ok(AtomVariant::Opaque(OpaqueAtom::new(id, flags, body)))
+            }
         }
     }
 
     pub fn write<W: Write>(&self, writer: &mut W) -> Result<(), AtomError> {
         let mut body = Vec::new();
-        match self {
-            AtomVariant::Null(a) => a.write(&mut body)?,
-            AtomVariant::Action(a) => a.write(&mut body)?,
-        }
+        let flags = match self {
+            AtomVariant::Opaque(a) => {
+                body.extend_from_slice(&a.body);
+                a.flags
+            }
+            AtomVariant::Action(a) => {
+                a.write(&mut body)?;
+                0
+            }
+        };
 
-        let id = self.id() as u32;
+        let id = self.id();
         writer.write_all(&id.to_le_bytes())?;
-        let size = body.len() as u64;
-        writer.write_all(&size.to_le_bytes())?;
+        let size = u64::try_from(body.len()).map_err(|_| AtomError::AtomTooLarge)?;
+        if size > 0x00ff_ffff_ffff_ffff {
+            return Err(AtomError::AtomTooLarge);
+        }
+        let size_and_flags = size | (u64::from(flags) << 56);
+        writer.write_all(&size_and_flags.to_le_bytes())?;
         writer.write_all(&body)?;
 
         Ok(())
